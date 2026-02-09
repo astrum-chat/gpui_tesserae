8fc07a65a74dc45f9e6ca156930b4677d6703e2f
Cameron Campbell <cameron.campbell325@gmail.com>
2026-02-09 09:34:29 +0000

    Move init_fonts into Assets impl block

--- src/assets/assets.rs
+++ src/assets/assets.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 
 use anyhow::anyhow;
-use gpui::{AssetSource, Result, SharedString};
+use gpui::{App, AssetSource, Result, SharedString};
 use smallvec::SmallVec;
 
 /// Composite asset source that queries multiple providers in order.
@@ -17,6 +17,28 @@ impl<const N: usize> Assets<N> {
         Self {
             providers: SmallVec::from(providers),
         }
+    }
+}
+
+impl Assets<0> {
+    /// Initializes font assets.
+    pub fn init_fonts(cx: &mut App) -> gpui::Result<()> {
+        let font_paths = cx.asset_source().list("fonts")?;
+        let mut embedded_fonts = Vec::new();
+
+        for font_path in font_paths {
+            if !font_path.ends_with(".ttf") {
+                continue;
+            }
+
+            let Some(font_bytes) = cx.asset_source().load(&font_path)? else {
+                continue;
+            };
+
+            embedded_fonts.push(font_bytes);
+        }
+
+        cx.text_system().add_fonts(embedded_fonts)
     }
 }
 
@@ -61,6 +83,7 @@ impl<const N: usize> AssetSource for Assets<N> {
 pub trait AssetProvider: Send + Sync {
     /// Returns the asset data at the given path, if it exists.
     fn get(&self, path: &str) -> Option<Cow<'static, [u8]>>;
+
     /// Lists all assets under the given path prefix.
     fn list(&self, path: &str) -> Result<Vec<SharedString>>;
 }
--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,7 @@
 use gpui::{App, Window};
 
 use crate::{
+    Assets,
     components::select,
     primitives::{input, selectable_text},
     theme::ThemeExt,
@@ -8,7 +9,7 @@ use crate::{
 
 /// Initializes global tesserae state. Call once at application startup.
 pub fn init(cx: &mut App) {
-    init_fonts(cx).expect("Could not initialize fonts!");
+    Assets::init_fonts(cx).expect("Could not initialize fonts!");
 
     input::init(cx);
     selectable_text::init(cx);
@@ -18,24 +19,4 @@ pub fn init(cx: &mut App) {
 /// Initializes per-window tesserae state. Call for each new window.
 pub fn init_for_window(window: &mut Window, cx: &mut App) {
     window.set_rem_size(cx.get_theme().layout.text.base_size);
-}
-
-/// Initializes font assets.
-pub fn init_fonts(cx: &mut App) -> gpui::Result<()> {
-    let font_paths = cx.asset_source().list("fonts")?;
-    let mut embedded_fonts = Vec::new();
-
-    for font_path in font_paths {
-        if !font_path.ends_with(".ttf") {
-            continue;
-        }
-
-        let Some(font_bytes) = cx.asset_source().load(&font_path)? else {
-            continue;
-        };
-
-        embedded_fonts.push(font_bytes);
-    }
-
-    cx.text_system().add_fonts(embedded_fonts)
 }
